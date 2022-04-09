e1e3ecde5139e5fe2c8a7786e097f6795dd36605
niha8515 <niha8515@yahoo.com>
2022-04-09 16:47:34 +0430

    add more questions to QuestionRepository.kt

--- app/src/main/java/com/example/livedatapractice/QuestionRepository.kt
+++ app/src/main/java/com/example/livedatapractice/QuestionRepository.kt
@@ -4,6 +4,10 @@ object QuestionRepository {
     val questionList = arrayListOf(
         " result of 2 + 2 " ,
         " result of  5 - 1 " ,
-        " result of 100 * 21"
+        " result of 100 * 21",
+        " result of 100 - 21",
+        " result of 100 + 21",
+        " result of 5 + 6",
+        " result of 10 * 21",
     )
 }
