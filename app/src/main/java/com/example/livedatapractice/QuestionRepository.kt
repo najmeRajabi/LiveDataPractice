909a1a91adf8db9091d43a0cf1a123a90a8f5f8c
niha8515 <niha8515@yahoo.com>
2022-04-09 19:41:46 +0430

    add data class Question

--- app/src/main/java/com/example/livedatapractice/QuestionRepository.kt
+++ app/src/main/java/com/example/livedatapractice/QuestionRepository.kt
@@ -2,12 +2,19 @@ package com.example.livedatapractice
 
 object QuestionRepository {
     val questionList = arrayListOf(
-        " result of 2 + 2 " ,
-        " result of  5 - 1 " ,
-        " result of 100 * 21",
-        " result of 100 - 21",
-        " result of 100 + 21",
-        " result of 5 + 6",
-        " result of 10 * 21",
+       Question( " result of 2 + 2 ",4) ,
+        Question(" result of  5 - 1 " ,4),
+        Question(" result of 100 * 21",2100),
+        Question(" result of 100 - 21",79),
+        Question(" result of 100 + 21",121),
+        Question(" result of 5 + 6",11),
+        Question(" result of 10 * 21",210),
     )
 }
+
+data class Question(
+    val question :String ,
+    val answer: Int ,
+    var answered : Boolean=false,
+    var correct: Boolean= false
+)
